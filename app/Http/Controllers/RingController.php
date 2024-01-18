c2f95b40ff03925151d5fb9a6635b9cc8aaf322e
mmbalbas <mmbalbasgolf@gmail.com>
2024-01-18 08:37:37 +0100

    comienzo 18 enero video12

---
+++ app/Http/Controllers/RingController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ring;
+use Illuminate\Http\Request;
+
+class RingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ring $ring)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Ring $ring)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Ring $ring)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ring $ring)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,50 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Models\Ring;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//Route::get('/rings', function (){
+//    return 'Bienvenido a Ring';
+//})->name('rings.index');
+
+//El middleware verified en estos momentos no realiza ninguna acción porque no está habilitado en el modelo Users (//use Illuminate\Contracts\Auth\MustVerifyEmail;)
+
+//Metí esta ruta dentro del grupo de abajo para que herede las propiedades del middlewarte auth. Le quito la llamada al método middleware auth y como solo retorno una vista, utilizo el método view y me queda como resultado Route::view('/dashboard', 'dashboard')->name('dashboard');
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
 
 Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-require __DIR__.'/auth.php';
+    Route::view('/rings', 'rings.index')->name('rings.index');
+
+    Route::post('/rings', function () {
+        // Insertar en la DB
+        Ring::create([
+            'mensaje' => request('mensaje'),
+            'user_id' => auth()->id(),
+
+    ]);
+
+        return redirect()->route('rings.index')->with('status', 'Ring created successfully');
+    });
+
+
+    });
+
+
+//    rings.index es la forma de blade de escribir la ruta (rings/index) Laravel ya detecta que está en la ruta resources/views
+
+
+
+
